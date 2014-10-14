1769e070213d49d00e9cfefd71373a7ab7949f84
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2014-10-14 22:14:56 +0100

    added multiple sizes of ship for the editor

--- src/client/ui/BoardDemo.java
+++ src/client/ui/BoardDemo.java
@@ -8,13 +8,15 @@ import java.awt.event.*;
 
 public class BoardDemo extends JFrame {
     //should ships be placed horizontally
+    int[] shipSizes = {2,3,3,4,5};
+    int currentSize = 0;
     boolean horizontal = false;
     public BoardDemo(){
         super("Board demo");
         setSize(new Dimension(320 , 320));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        int unitSize = 32;
+        final int unitSize = 32;
         final BattleBoard board = new BattleBoard();
         getLayeredPane().add(board, new Integer(1));
 
@@ -23,18 +25,28 @@ public class BoardDemo extends JFrame {
         panel.setOpaque(false);
         panel.setSize(320, 320);
 
+
+
+
+
         //the ship the user can move
+
+        final JPanel shipContainer = new JPanel();
+        shipContainer.setLayout(new BoxLayout(shipContainer,BoxLayout.Y_AXIS));
+
         final BattleAnimationPanel front = new BattleAnimationPanel(unitSize,unitSize);
 
-        final BattleAnimationPanel middle = new BattleAnimationPanel(unitSize,unitSize);
+        //final BattleAnimationPanel middle = new BattleAnimationPanel(unitSize,unitSize);
 
         final BattleAnimationPanel bottom = new BattleAnimationPanel(unitSize,unitSize);
         bottom.setAsShipEnd(BattleAnimationPanel.SOUTH);
         front.setAsShipEnd(BattleAnimationPanel.NORTH);
-        middle.setAsShipMiddle(BattleAnimationPanel.VERTICAL);
-        panel.add(front);
-        panel.add(middle);
-        panel.add(bottom);
+        //middle.setAsShipMiddle(BattleAnimationPanel.VERTICAL);
+        shipContainer.add(front);
+        //shipContainer.add(middle);
+        shipContainer.add(bottom);
+        panel.add(shipContainer);
+        shipContainer.setOpaque(false);
         panel.setVisible(false);
 
         setFocusable(true);
@@ -45,7 +57,20 @@ public class BoardDemo extends JFrame {
             public void keyTyped(KeyEvent e) {
                 super.keyTyped(e);
                 if (e.getKeyChar() == 'r') {
+                    System.out.println("key");
                     horizontal = !horizontal;
+                    //shipContainer.setOpaque(true);
+                    //shipContainer.setBackground(Color.red);
+                    shipContainer.setLayout(new BoxLayout(shipContainer, horizontal ? BoxLayout.X_AXIS : BoxLayout.Y_AXIS));
+                    shipContainer.setBounds(shipContainer.getX(), shipContainer.getY(),
+                            horizontal ? shipContainer.getComponentCount() * 32 : 32,
+                            horizontal ? 32 : shipContainer.getComponentCount() * 32);
+                    front.setAsShipEnd(!horizontal? BattleAnimationPanel.NORTH : BattleAnimationPanel.WEST);
+                    for(Component c : shipContainer.getComponents()){
+                        if(c!= front && c!= bottom) ((BattleAnimationPanel)c).setAsShipMiddle(horizontal?BattleAnimationPanel.HORIZONTAL:BattleAnimationPanel.VERTICAL);
+                    }
+                    bottom.setAsShipEnd(!horizontal? BattleAnimationPanel.SOUTH : BattleAnimationPanel.EAST);
+                    shipContainer.doLayout();
                 }
             }
         });
@@ -63,7 +88,7 @@ public class BoardDemo extends JFrame {
                 int x3 = (horizontal)?e.getX()+16+32:x1;
 
                 int y1 = (horizontal)?e.getY()-8:e.getY()-8;
-                int y2 = (horizontal)?y1 : e.getY()-8+32;
+                /*int y2 = (horizontal)?y1 : e.getY()-8+32;
                 int y3 = (horizontal)?y1:e.getY()-8+32+32;
                 front.setBounds(x1,y1, 32, 32);
                 middle.setBounds(x2,y2 , 32, 32);
@@ -71,7 +96,12 @@ public class BoardDemo extends JFrame {
 
                 front.setAsShipEnd(horizontal ? BattleAnimationPanel.WEST : BattleAnimationPanel.NORTH);
                 middle.setAsShipMiddle( horizontal ? BattleAnimationPanel.HORIZONTAL : BattleAnimationPanel.VERTICAL);
-                bottom.setAsShipEnd(horizontal ? BattleAnimationPanel.EAST : BattleAnimationPanel.SOUTH);
+                bottom.setAsShipEnd(horizontal ? BattleAnimationPanel.EAST : BattleAnimationPanel.SOUTH);*/
+
+                shipContainer.setBounds(x1,y1,
+                        horizontal? shipContainer.getComponentCount() * 32 : 32 ,
+                        horizontal? 32: shipContainer.getComponentCount() * 32);
+
                 //this was supposed to highlight the squares that are selected
                 //but swing is ignoring the event when i do :|
                 //int x = (int)Math.floor( e.getX() / 32.0);
@@ -116,11 +146,32 @@ public class BoardDemo extends JFrame {
                    board.tiles[x2][y2].object.getIcon() !=null){
                     return;
                 }
+                int current = shipSizes[currentSize];
 
                 //set the actual tiles on the board
                 board.tiles[x][y].setAsShipEnd(horizontal ? BattleAnimationPanel.WEST : BattleAnimationPanel.NORTH);
-                board.tiles[x1][y1].setAsShipMiddle( horizontal ? BattleAnimationPanel.HORIZONTAL : BattleAnimationPanel.VERTICAL);
-                board.tiles[x2][y2].setAsShipEnd(horizontal ? BattleAnimationPanel.EAST : BattleAnimationPanel.SOUTH);
+                for(int r =0;r<current-2;r++){
+                    board.tiles[x+ (r+1) * (!horizontal?0:1)][y + (r+1) * (horizontal?0:1)].setAsShipMiddle(horizontal ? BattleAnimationPanel.HORIZONTAL : BattleAnimationPanel.VERTICAL);
+                }
+                //board.tiles[x1][y1].setAsShipMiddle( horizontal ? BattleAnimationPanel.HORIZONTAL : BattleAnimationPanel.VERTICAL);
+                board.tiles[x+ (current-2+1) * (!horizontal?0:1)][y + (current-2+1) * (horizontal?0:1)].setAsShipEnd(horizontal ? BattleAnimationPanel.EAST : BattleAnimationPanel.SOUTH);
+
+                currentSize++;
+                if(currentSize > 4){
+                    shipContainer.removeAll();
+                    return;
+                }
+                current = shipSizes[currentSize];
+                shipContainer.remove(bottom);
+                for(Component c: shipContainer.getComponents()){
+                    if(c != front) shipContainer.remove(c);
+                }
+                for(int r = 0; r < current-2;r++) {
+                    BattleAnimationPanel panel = new BattleAnimationPanel(unitSize,unitSize);
+                    panel.setAsShipMiddle(horizontal ? BattleAnimationPanel.HORIZONTAL : BattleAnimationPanel.VERTICAL);
+                    shipContainer.add(panel);
+                }
+                shipContainer.add(bottom);
             }
         });
 
