0d8244ce81c6c67429b077b3f9e25f181c811869
Mark Ormesher <me@markormesher.co.uk>
2014-10-14 14:29:46 +0100

    Added game-specific animation panel

---
+++ src/client/ui/BattleAnimationPanel.java
@@ -0,0 +1,70 @@
+package client.ui;
+
+public class BattleAnimationPanel extends AnimationPanel {
+
+	// constants
+	public static final int NORTH = 1,
+	EAST = 2,
+	SOUTH = 3,
+	WEST = 4,
+	HORIZONTAL = 5,
+	VERTICAL = 6;
+
+	public BattleAnimationPanel(int width, int height) {
+		super(width, height);
+	}
+
+	// set this panel as a ship end section
+	public void setAsShipEnd(int orientation) {
+		// find rotation
+		int angle;
+		switch (orientation) {
+			case NORTH:
+				angle = 0;
+				break;
+			case EAST:
+				angle = 90;
+				break;
+			case SOUTH:
+				angle = 180;
+				break;
+			case WEST:
+				angle = 270;
+				break;
+			default:
+				angle = 0;
+				break;
+		}
+
+		// set image
+		setObject("ship-end", angle);
+	}
+
+	// set this panel as a ship middle section
+	public void setAsShipMiddle(int orientation) {
+		// find rotation
+		int angle;
+		switch (orientation) {
+			case VERTICAL:
+				angle = 0;
+				break;
+			case HORIZONTAL:
+			default:
+				angle = 90;
+				break;
+		}
+
+		// set image
+		setObject("ship-middle", angle);
+	}
+
+	// set this panel as a "hit" pin
+	public void setAsHitPin() {
+		setObject("pin-hit", 0);
+	}
+
+	// set this panel as a "miss" pin
+	public void setAsMissPin() {
+		setObject("pin-miss", 0);
+	}
+}
