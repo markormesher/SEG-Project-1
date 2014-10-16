cfcd4cb801f3da3a751b54da0d8ffab3711b150d
Parin Parikh <parin_parikh@yahoo.co.uk>
2014-10-16 12:10:33 +0100

    Made loops more efficient by adding break statements on match found

--- src/server/BattleServer.java
+++ src/server/BattleServer.java
@@ -211,7 +211,7 @@ public class BattleServer {
 				if (msg.getType() == Message.SET_USERNAME) {
 					// TODO: enforce unique usernames (server could send reply to client, either USERNAME_OK or USERNAME_TAKEN)
 					// set username of this thread
-					username = msg.getMessage();
+					username = msg.getMessage()
 					checkBothUsernameSet(id);
 
 					// notify all clientConnectedListeners
@@ -227,6 +227,8 @@ public class BattleServer {
 							} catch (IOException e) {
 								serverMessageListener.onServerMessageReceived("Failed to send message to " + msg.getRecipient());
 							}
+
+              break;
 						}
 					}
 				}
